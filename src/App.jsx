f4d2b1e1d740ef735700b87d41ff3cfa06f55e13
Bhoomikarangaraj <24bcs107@kgcas.com>
2026-01-03 12:43:41 +0530

    program 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [submitData,setSubmitData]=useState(null)
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const formdata={name,email,password};
+    setSubmitData(formdata);
+    setName("")
+    setEmail("")
+    setPassword("")
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Form</h2>
+      <form onSubmit={handleSubmit}>
+         <label>Name:</label>
+         <input type="text"
+         value={name}
+         onChange={(e)=>setName(e.target.value)}
+         required />
+         <label>Email:</label>
+         <input type="email"
+         value={email}
+         onChange={(e)=>setEmail(e.target.value)}
+         required />
+         <label>Password:</label>
+         <input type="password"
+         value={password}
+         onChange={(e)=>setPassword(e.target.value)}
+         required/>
+
+         <button type="submit">Submit</button>
+         </form>
+         {submitData &&(
+           <div>
+            <p><strong>Name:</strong>
+            {submitData.name}</p>
+            <p><strong>Email:</strong>
+            {submitData.email}</p>
+            <p><strong>Password:</strong>
+            {submitData.password}</p>
+           </div>
+         )}
+
+         
+    </div>
   )
 }
 
 export default App
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TodoList from './todolist.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TodoList />
+    <App />
   </StrictMode>,
 )
